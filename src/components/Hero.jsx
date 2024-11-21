d642e9490ceab0c96e0dcfd620165047026fbb70
Altairian7 <artisharma.as12.as@gmail.com>
2024-11-21 21:13:59 +0530

    added a line in hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -32,7 +32,7 @@ const Hero = () => {
     <section className="w-full nav-height bg-black relative">
       <div className="h-5/6 w-full flex-center flex-col">
         <p id="hero" className="hero-title">
-          iPhone 15 pro
+          IPhone 19 Ultra Pro Max
         </p>
         <div className="md:w-10/12 w-9/12">
           <video
